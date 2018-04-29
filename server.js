71f1c52e6f9bcb14f2ffecb8c2cf8983d634b632
Sathish Parameshwara <Sathish.Parameshwara@aexp.com>
2018-04-29 13:25:37 +0530

    heroku setup-1

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use((req,res,next) => {
     next();
 });
 
-app.use((req,res,next) => {
-    res.render('maintenace.hbs');
-});
+// app.use((req,res,next) => {
+//     res.render('maintenace.hbs');
+// });
 
 app.use(express.static(__dirname+'/public'));
 
